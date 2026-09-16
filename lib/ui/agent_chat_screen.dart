5941362a000d0656e8dd9826cb5e2df4b8c1940b
iggdigd218-dev <iggdigd218-dev@users.noreply.github.com>
2026-09-16 09:05:48 +0000

    fix: سطر استيراد Clipboard

--- lib/ui/agent_chat_screen.dart
+++ lib/ui/agent_chat_screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/services.dart' show Clipboard, ClipboardData;\nimport 'dart:async';
+import 'package:flutter/services.dart' show Clipboard, ClipboardData;
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 
